296decc5a40cf5e8fbbeeaaba58989bc89f373c3
yougataka <64336275+yougataka@users.noreply.github.com>
2020-06-29 11:10:21 -0100

    Add files via upload

---
+++ first python program with IDLE.py
@@ -0,0 +1,8 @@
+from math import sqrt
+
+x = input('spell the word (python is the best programming language ever) in uppercase and without space!!:  ')
+if x == 'PYTHONISTHEBESTPROGRAMMINGLANGUAGEEVER':
+    print('true you passed the first test ')
+y =( input('find the solutions when x = 0 ; x**2+2x-1 then put the result into this input field :  '))
+if y == -1+(sqrt(20)) or y == -1-(sqrt(20)):
+    print('True mate thank you for trying my first program')
